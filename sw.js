14305f6f25dafb23c6d9c31b5cf284cb104e6327
yeltnar <andcbrant@gmail.com>
2017-12-06 14:34:40 -0600

    caching more things

--- sw.js
+++ sw.js
@@ -1,4 +1,24 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/staticFiles/bread.png',
+  '/staticFiles/home.svg',
+  '/staticFiles/icon.png',
+  '/staticFiles/icon2.png',
+  '/staticFiles/icon3.png',
+  '/staticFiles/main.js',
+  '/staticFiles/recipes.css',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
 self.addEventListener('install', function(event) {
   // Perform install steps
-  debugger;
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
